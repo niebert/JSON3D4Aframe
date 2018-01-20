ece65a77c6a0de339afd9f1cef4a1230fc21d7d7
Engelbert Niehaus <niehaus@uni-landau.de>
2018-01-20 16:45:58 +0100

    FileSaver.js updated

---
+++ update_filesaver.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/eligrey/FileSaver.js/master/FileSaver.js -O docs/js/filesaver.js
+wget https://raw.githubusercontent.com/eligrey/Blob.js/master/Blob.js -O docs/js/blob.js
