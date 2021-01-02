716685f957fa807378032c83bcefca8765112312
Engelbert Niehaus <niehaus@uni-landau.de>
2021-01-02 18:13:04 +0100

    README updated

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 /* ---------------------------------------
  Exported Module Variable: JSON3D4Aframe
  Package:  json3d4aframe
- Version:  2.0.1  Date: 2021/01/02 18:06:17
+ Version:  2.0.1  Date: 2021/01/02 18:12:29
  Homepage: https://github.com/niebert/JSON3D4Aframe#readme
  Author:   Engelbert Niehaus
  License:  MIT
- Date:     2021/01/02 18:06:17
+ Date:     2021/01/02 18:12:29
  Require Module with:
     const JSON3D4Aframe = require('json3d4aframe');
  JSHint: installation with 'npm install jshint -g'
@@ -1416,11 +1416,11 @@ RegExp.escape = function (s) {
 /* ---------------------------------------
  Exported Module Variable: JSON3D4Aframe
  Package:  json3d4aframe
- Version:  2.0.1  Date: 2021/01/02 18:06:17
+ Version:  2.0.1  Date: 2021/01/02 18:12:29
  Homepage: https://github.com/niebert/JSON3D4Aframe#readme
  Author:   Engelbert Niehaus
  License:  MIT
- Date:     2021/01/02 18:06:17
+ Date:     2021/01/02 18:12:29
  Require Module with:
     const JSON3D4Aframe = require('json3d4aframe');
  JSHint: installation with 'npm install jshint -g'
