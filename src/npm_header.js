d0b7daa0e43d02d6e21fb522c6b3b68e3bc5cd13
Engelbert Niehaus <niehaus@uni-landau.de>
2021-01-03 12:38:37 +0100

    JSON3D4Aframe update for FileSaver

--- src/npm_header.js
+++ src/npm_header.js
@@ -1,11 +1,11 @@
 /* ---------------------------------------
  Exported Module Variable: JSON3D4Aframe
  Package:  json3d4aframe
- Version:  2.0.1  Date: 2021/01/02 18:12:29
+ Version:  2.0.2  Date: 2021/01/03 11:34:55
  Homepage: https://github.com/niebert/JSON3D4Aframe#readme
  Author:   Engelbert Niehaus
  License:  MIT
- Date:     2021/01/02 18:12:29
+ Date:     2021/01/03 11:34:55
  Require Module with:
     const JSON3D4Aframe = require('json3d4aframe');
  JSHint: installation with 'npm install jshint -g'
