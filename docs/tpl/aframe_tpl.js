0255620431fba08d90b953fe10899fe17bde1e35
Engelbert Niehaus <niehausbert@gmail.com>
2021-12-13 17:31:08 +0100

    Update aframe_tpl.js
    tags wrong format "&gt;" and  "&lt;" instead "<" and ">"

--- docs/tpl/aframe_tpl.js
+++ docs/tpl/aframe_tpl.js
@@ -1,19 +1,19 @@
 vDataJSON.tpl.aframe = `
-&lt;!DOCTYPE html&gt;
-&lt;html&gt;
-  &lt;head&gt;
-    &lt;title&gt;A-Frame - {{titlemodel}} &lt;/title&gt;
-    &lt;meta name="description" content="A-Frame {{titlemodel}}"&gt;
-    &lt;meta name="createdwith" content="https://niebert.githhub.io/JSON3D4Aframe"&gt;
-    &lt;script src="https://aframe.io/releases/1.1.0/aframe.min.js"&gt;&lt;/script&gt;
-  &lt;/head&gt;
-  &lt;body&gt;
-    &lt;a-scene&gt;
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>A-Frame - {{titlemodel}} </title>
+    <meta name="description" content="A-Frame {{titlemodel}}">
+    <meta name="createdwith" content="https://niebert.githhub.io/JSON3D4Aframe">
+    <script src="https://aframe.io/releases/1.1.0/aframe.min.js"></script>
+  </head>
+  <body>
+    <a-scene>
 {{{arobjects}}}
 {{{plane}}}
 {{{sky}}}
 {{{cameraposition}}}
-    &lt;/a-scene&gt;
-  &lt;/body&gt;
-&lt;/html&gt;
+    </a-scene>
+  </body>
+</html>
 `;
