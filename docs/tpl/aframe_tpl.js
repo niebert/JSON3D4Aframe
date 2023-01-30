77cc800882c02c1c7af3bc3b583b1d9184903865
Engelbert Niehaus <niehausbert@gmail.com>
2023-01-30 09:53:32 +0100

    Update aframe_tpl.js

--- docs/tpl/aframe_tpl.js
+++ docs/tpl/aframe_tpl.js
@@ -5,8 +5,9 @@ vDataJSON.tpl.aframe = `
     <title>A-Frame - {{titlemodel}} </title>
     <meta name="description" content="A-Frame {{titlemodel}}">
     <meta name="createdwith" content="https://niebert.githhub.io/JSON3D4Aframe">
-    <script src="https://aframe.io/releases/1.1.0/aframe.min.js"></script>
-  </head>
+     <!-- <script src="https://aframe.io/releases/1.1.0/aframe.min.js"></script> -->
+    <script src="./js/aframe-v1.0.4.js"></script>
+ </head>
   <body>
     <a-scene>
 {{{arobjects}}}
