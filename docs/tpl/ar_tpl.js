263705fd6522ddfed8a31f394f4b9d6865967c1d
Engelbert Niehaus <niehausbert@gmail.com>
2023-01-30 09:53:04 +0100

    Update ar_tpl.js

--- docs/tpl/ar_tpl.js
+++ docs/tpl/ar_tpl.js
@@ -3,9 +3,13 @@ vDataJSON.tpl.ar = `
 <html>
     <head>
       <meta name="createdwith" content="https://niebert.githhub.io/JSON3D4Aframe">
+        <!--
       <script src="https://aframe.io/releases/1.1.0/aframe.min.js"></script>
       <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.5.0/aframe/build/aframe-ar.js"> </script>
-      <title>
+      -->
+      <script src="./js/aframe-v1.0.4.js"></script>
+      <script src="./js/aframe-ar.js"></script>
+    <title>
             AR {{marker}} Marker - {{titlemodel}}
       </title>
     </head>
