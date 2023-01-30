6a6f683db37005128263f1bb36e5c4d3d5dd1616
Engelbert Niehaus <niehausbert@gmail.com>
2023-01-30 13:45:25 +0100

    Add files via upload

---
+++ docs/tpl/aframe4save_tpl.js
@@ -0,0 +1,20 @@
+vDataJSON.tpl.aframe4save = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>A-Frame - {{titlemodel}} </title>
+    <meta name="description" content="A-Frame {{titlemodel}}">
+    <meta name="createdwith" content="https://niebert.githhub.io/JSON3D4Aframe">
+    <script src="https://aframe.io/releases/1.1.0/aframe.min.js"></script>
+    <!-- <script src="./js/aframe-v1.0.4.js"></script> -->
+  </head>
+  <body>
+    <a-scene>
+{{{arobjects}}}
+{{{plane}}}
+{{{sky}}}
+{{{cameraposition}}}
+    </a-scene>
+  </body>
+</html>
+`;
